66394329ccbf76b07a377555748acb08ceef6015
Andrew <Andrew@HOME-PC>
2021-10-25 00:11:42 +0300

    Add project files.

---
+++ ClassLibrary1/ArrayWorker.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace ClassLibrary
+{
+    public class ArrayWorker
+    {
+        public int[] InvertArray(int[] nums)
+        {
+            int[] invNums = new int[nums.Length];
+            for (int i = 0; i < invNums.Length; i++)
+            {
+                invNums[i] = nums[nums.Length-1-i];
+            }
+            return invNums;
+        }
+        public void ShowArray(string text, int[] nums)
+        {
+            Console.Write(text);
+            for (int i = 0; i < nums.Length-1; i++)
+            {
+                Console.Write($"{nums[i]}, ");
+            }
+            Console.Write($"{nums[nums.Length-1]}.");
+        }
+
+        public int[] AddValue(int[] nums, int value, int index)
+        {
+            if (index <= nums.Length)
+            {
+                int[] upd_nums = new int[nums.Length + 1];
+                for (int i = 0; i < index; i++)
+                {
+                    upd_nums[i] = nums[i];
+                }
+                upd_nums[index] = value;
+                for (int i = index + 1; i <= nums.Length; i++)
+                {
+                    upd_nums[i] = nums[i - 1];
+                }
+                return upd_nums;                
+            }
+            //Если в массив вставляется число с индексом большим чем +1 к длине массива, то промежуток заполняется нулями.
+            else
+            {                
+                int[] upd_nums = new int[index];
+                for (int i = 0; i < nums.Length; i++)
+                {
+                    upd_nums[i] = nums[i];
+                }
+                //for (int i = nums.Length; i < upd_nums.Length; i++)
+                //{
+                //    upd_nums[i] = 0;
+                //}
+                upd_nums[index-1] = value;
+                return upd_nums;
+            }       
+                        
+        }
+    }
+
+    public class Cone
+    {
+        public int Radius;
+        public int Height;
+        public Cone(int r,int h)
+        {
+            Radius = r;
+            Height = h;
+        }
+        public double BaseSquare()
+        {
+            return Math.Round((Math.PI * Math.Pow(Radius, 2)),3);
+        }
+        public double AllSquare()
+        {
+            return Math.Round((Math.PI * Radius * Height), 3);
+        }
+    }
+}
---
+++ Exercise5/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using ClassLibrary;
+
+namespace Exercise5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задание 1, 2
+            int[] numbers = new int[] { 6,3,5,41};
+            ArrayWorker Worker = new ArrayWorker();
+            Worker.ShowArray("Original array: ",numbers);
+            Console.WriteLine();
+            int[] inv_nums = Worker.InvertArray(numbers);
+            Worker.ShowArray("Inverted array: ", inv_nums);
+            Console.WriteLine();
+            Console.Write("Enter value: ");
+            int value = int.Parse(Console.ReadLine());
+            Console.Write("Enter index: ");
+            int index = int.Parse(Console.ReadLine());
+            int[] upd_nums = Worker.AddValue(numbers, value, index);
+            Worker.ShowArray("Updated array: ",upd_nums);
+            Console.WriteLine();
+
+            //Задание 3
+            Cone myCone = new Cone(5,20);
+            Console.WriteLine($"Square of base = {myCone.BaseSquare()} cm2");
+            Console.WriteLine($"All square = {myCone.AllSquare()} cm2");
+
+        }
+    }
+}
